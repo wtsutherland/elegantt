80c4a848d7d5e4e2b8a50e11e9cd3e9456c9116b
wtsutherland <ntk688@gmail.com>
2023-08-02 22:26:41 -0700

    Implement inelegant version of reading CSV file and incomplete xlimits method. Add tests directory and test CSV.

--- src/elegantt.py
+++ src/elegantt.py
@@ -1,19 +1,56 @@
 import csv
+import sys
+import json
+import matplotlib.pyplot as plt
+import datetime
+
+arg = sys.argv
 
 class Timeline:
-    def __init__(self):
+    def __init__(self, data_path=None):
         self.data = None
         self.title = None
+        self.data  = None
+
+        if data_path:
+            self.data = self.read(data_path)
+        
+        if self.data:
+            self.xlimits()
 
     def read(self, path):
-        self.data = {}
+        data = {}
 
         with open(path, 'r') as data_file:
             for row in csv.reader(data_file):
 
                 # Logic to assign each row as either periods or events
                 # in data structure.
-                if row[2] != None or row[2] != 0:
-                    data[row[0]] = (row[1], row[2])
+                if row[2]:
+
+                    if isinstance(row[2], str):
+                        if row[2].lower() == "present":
+                            now = datetime.datetime.now()
+                            data[row[0]] = (row[1], now)
+                    else:
+                        data[row[0]] = (row[1], row[2])
                 else:
                     data[row[0]] = row[1]
+
+        return data
+
+
+    def xlimits(self):
+        times = []
+
+        for key, value in self.data.items():
+            if isinstance(value, str):
+                times.append(value)
+            elif isinstance(value, tuple):
+                times += value
+
+        print(times)
+            
+        
+
+tm = Timeline(data_path="../tests/test_data.csv")
